47630371f39ed12fdf16c95de9f98124ae026ad4
7774j <80786885+7774j@users.noreply.github.com>
2021-06-02 20:05:15 -0500

    Attempting on my own
    Attempting my own solution thus far with the help of Max's previous lesson and code along.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormArray, FormGroup, FormBuilder, FormControl, Validators, } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  forbiddenProjectNames = ['Test'];
+  signupForm: FormGroup;
+
+
+  constructor (private formBuilder: FormBuilder) {
+
+  }
+  ngOnInit(){
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'projectName' : new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        'email' : new FormControl(null, [Validators.required, Validators.email])
+      })
+    })
+  }
+  onSubmit() {
+    console.log(this.signupForm);
+   }
+   forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
+  }
 }
+
